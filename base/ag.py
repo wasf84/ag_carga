4641fe9bc5fde08ec9584afc4e6433e1482841ef
wasf84 <wfilho@ice.ufjf.br>
2015-07-03 12:35:32 -0300

    Fazendo busca na geracao atual e descobrindo quem eh o melhor individuo.

--- base/ag.py
+++ base/ag.py
@@ -8,6 +8,7 @@ class AG:
     geracao = None
     populacao = []
 
+    # ------------------------------------ #
     def __init__(self):
         """
         Vai gerar a populacao inicial aqui dentro.
@@ -18,6 +19,7 @@ class AG:
             ind = Individuo()
             self.populacao.append(ind)
 
+    # ------------------------------------ #
     def printTudo(self):
         """
         Imprime tudo que contiver no AG
@@ -26,5 +28,24 @@ class AG:
         print "Geracao: " + str(self.geracao)
         for i in range(0, len(self.populacao)):
             print "x ---------------------- x"
-            print "ID: " + str(i+1)
+            print "ID: " + str(i)
             self.populacao[i].printIndividuo()
+
+    # ------------------------------------ #
+    def printUmEspecifico(self, index):
+        # print "Geracao: " + str(self.geracao)
+        if index in range(0, len(self.populacao)):
+            print "x ---------------------- x"
+            print "ID: " + str(index)
+            self.populacao[index].printIndividuo()
+            print "x ---------------------- x"
+
+    # ------------------------------------ #
+    def selecionarMelhorGeracao(self):
+        # Primeiro eu pego os individuos que estao dentro do limite de carga do aviao, que eh de 5 toneladas.
+        listaMelhores = []
+        for i in range(0, len(self.populacao)):
+            if self.populacao[i].retornaCargaTotal() <= 5:
+                listaMelhores.append(self.populacao[i])
+
+        # Agora pego quem tiver a maior utilidade.
--- base/individuo.py
+++ base/individuo.py
@@ -10,6 +10,17 @@ class Individuo:
     carga = None
 
     # ------------------------------------ #
+    def __init__(self):
+        # Gerando uma lista binaria aleatoria
+        # fonte: <http://code.activestate.com/recipes/577944-random-binary-list/>
+        randBinList = lambda n: [randint(0, 1) for b in range(1, n+1)]
+
+        self.cromossomo = randBinList(8)
+        self.calcularUtilidade()
+        self.calcularCargaTotal()
+        # self.printIndividuo()
+
+    # ------------------------------------ #
     def calcularUtilidade(self):
         """
         Funcao de Avaliacao (conceito geral de AG) que calcularah a utilidade do individuo em questao
@@ -64,14 +75,3 @@ class Individuo:
         print "Cromossomo: " + str(self.cromossomo)
         print "Utilidade: " + str(self.utilidade)
         print "Carga Total: " + str(self.carga)
-
-    # ------------------------------------ #
-    def __init__(self):
-        # Gerando uma lista binaria aleatoria
-        # fonte: <http://code.activestate.com/recipes/577944-random-binary-list/>
-        randBinList = lambda n: [randint(0, 1) for b in range(1, n+1)]
-
-        self.cromossomo = randBinList(8)
-        self.calcularUtilidade()
-        self.calcularCargaTotal()
-        # self.printIndividuo()
